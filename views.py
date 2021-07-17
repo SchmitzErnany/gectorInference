7d1d4316dd10971109b04e01fcd44376df9ea2d1
SchmitzErnany <ernany_schmitz@yahoo.com.br>
2021-07-17 01:17:17 -0300

    first commit

---
+++ views.py
@@ -0,0 +1,91 @@
+# -*- coding: utf-8 -*-
+
+"""
+This script obtains the replacement pair, along with the
+position and length of the input woprd in the input sentence.
+Additionally, every other information for the JSON output is
+defined.
+"""
+
+from gectorPredict.predict import predict_for_paragraph
+from gectorPredict.gector.gec_model import GecBERTModel
+
+args = {'vocab_path':'gectorPredict/MODEL_DIR/vocabulary', 'model_path':['gectorPredict/MODEL_DIR/best.th'],
+        'max_len':50,'min_len':3,'iteration_count':5,'min_error_probability':0.0,
+        'lowercase_tokens':0,'transformer_model':'bertimbau','special_tokens_fix':1,'additional_confidence':0,
+        'is_ensemble':0,'weights':None,'input_file':'eval_after_train.txt','output_file':'OUTPUT_FILE.txt'}
+
+model = GecBERTModel(model_paths=args['model_path'],
+                     vocab_path=args['vocab_path'],
+                     max_len=args['max_len'], min_len=args['min_len'],
+                     iterations=args['iteration_count'],
+                     min_error_probability=args['min_error_probability'],
+                     lowercase_tokens=args['lowercase_tokens'],
+                     model_name=args['transformer_model'],
+                     special_tokens_fix=args['special_tokens_fix'],
+                     log=False,
+                     confidence=args['additional_confidence'],
+                     is_ensemble=args['is_ensemble'],
+                     weigths=args['weights'])
+
+#%%
+
+request_string = '   A diretoria feia estúpida e arrogante foram embora. Ele vão embora.'
+repl = predict_for_paragraph(request_string, model, batch_size=32)
+
+
+
+json_output = dict()
+json_output['software'] = {'deep3SPVersion':'0.1'}
+json_output['warnings'] = {'incompleteResults':False}
+json_output['language'] = {'name':'Portuguese (Deep SymFree)'}
+json_output['matches'] = []
+for i, (key, value) in enumerate(zip(repl.keys(), repl.values())):
+    match_dict = dict()
+    match_dict['message'] = 'Possível erro de concordância. Considere a alternativa.'
+    match_dict['incorrectExample'] = 'As pessoas foi para casa.'
+    match_dict['correctExample'] = 'As pessoas foram para casa.'
+    match_dict['shortMessage'] = 'Modifique a forma verbal'
+    match_dict['replacements'] = [{'value':value[2]}]
+    match_dict['offset'] = value[0]
+    match_dict['length'] = value[1]
+    match_dict['context'] = {'text':request_string, 'offset':value[0], 'length':value[1]}
+    match_dict['sentence'] = request_string
+    match_dict['type'] = {'typeName':'Hint'}
+    match_dict['rule'] = {'id':'DEEP_VERB_3SP', 'subId':0, 'sourceFile': 'not well defined', 'description': 'Deep learning rules for the 3rd person Singular-Plural', 'issueType':'grammar', 'category':{'id':'SymFree_DEEP_1' , 'name':'Deep learning rules (SymFree 1)'}}
+    match_dict['ignoreForIncompleteSentence'] = False
+    match_dict['contextForSureMatch'] = -1
+    json_output['matches'].append(match_dict)
+
+json_output
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
